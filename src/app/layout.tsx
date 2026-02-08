9a742ee70dd50b2ad44cc8396ec941f9b97eb564
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-02-08 17:36:25 -0300

    Instalo dependencias y mas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/theme';
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Agus Santinelli | Portfolio",
