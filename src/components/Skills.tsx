86daca78418a3869cb631e903d46bfeb3e5a4969
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-03-16 01:10:44 -0300

    Actualizo Skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -31,7 +31,7 @@ const skills = [
     },
     {
         category: 'Testing Ecosystem',
-        items: ['Playwright (E2E)', 'Vitest', 'MSW', 'Faker.js', 'Jest', 'Jasmine', 'Karma']
+        items: ['Playwright (E2E)', 'Cypress (E2E)', 'Vitest', 'MSW', 'Faker.js', 'Jest', 'Jasmine', 'Karma']
     },
     {
         category: 'DevOps & VCS',
